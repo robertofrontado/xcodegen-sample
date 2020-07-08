0b4916f47f7f6abc3a7896aa4ee24759d8c132d3
Roberto Frontado <robertofrontado@gmail.com>
2020-07-08 22:28:37 +0200

    First commit

---
+++ XcodeGenSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeGenSample
+//
+//  Created by Roberto Frontado on 08/07/2020.
+//  Copyright © 2020 Roberto Frontado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XcodeGenSampleTests/XcodeGenSampleTests.swift
@@ -0,0 +1,32 @@
+//
+//  XcodeGenSampleTests.swift
+//  XcodeGenSampleTests
+//
+//  Created by Roberto Frontado on 08/07/2020.
+//
+
+import XCTest
+
+class XcodeGenSampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
